333b3cb1eb6dc24f5955aad7ad42e05920bdc7f7
Trace <tracedelange@me.com>
2021-08-24 15:59:52 -0500

    commit tree d0ff206dd52a11b11255bbd907634df9fcd9361e

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+
+  belongs_to :network
+
+  has_many :characters
+  has_many :actors, through: :characters
+
+
+  def actors_list
+    self.actors.all.map {|item| item.first_name + ' ' + item.last_name}
+  end
+
+
 end
